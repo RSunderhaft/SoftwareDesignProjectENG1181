5f63f4f7a1be27c1c12bbfcd5827b434b1aa6a95
Dylan Williams <74433987+dwill-osu@users.noreply.github.com>
2020-11-30 15:52:16 -0500

    Update battleShip.m

--- battleShip.m
+++ battleShip.m
@@ -42,7 +42,7 @@ drawScene(my_scene,board_display,hitmiss_display)
 %% Set board for opponent and player
 opponentLevel = input('AI Difficulty(Enter 0 for easy mode and 1 for Normal): '); %0 represents easy mode while 1 represents medium mode
  if (opponentLevel<0 || opponentLevel>1)
-            fprintf('Error: Not valid Level');
+            fprintf('Error: Not valid Level \n');
             while(opponentLevel<0 || opponentLevel>1)
             opponentLevel = input('Reselect AI Difficulty: ');
             end

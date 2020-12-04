6e55b47d6c6927c766faf1defaf0993f2ef74bf0
RSunderhaft <67446641+RSunderhaft@users.noreply.github.com>
2020-12-04 16:11:08 -0500

    Update displaySunkShips.m

--- displaySunkShips.m
+++ displaySunkShips.m
@@ -1,10 +1,16 @@
 function [board_display] = displaySunkShips(shipType, opponentShips, opponent_ship_array, board_display)
-    
-    for i = 1:10
+    % Description: This will dispay individual ships once the user sinks
+    % them
+    % How it works: This method will search for the specific ship sunk,
+    % retrive the cordnates, then will displays ship parts on those
+    % cordinates.
+
+    %Nested for loops to search entire board
+    for i = 1:10 
         for j = 1:10
-            if (opponentShips(i,j) == shipType) % Check to make sure it is correct shipt to display
-                shipPartType = opponent_ship_array(i,j);
-                board_display(i,j+11) = shipPartType;
+            if (opponentShips(i,j) == shipType) % Check to make sure it is correct ship to display
+                shipPartType = opponent_ship_array(i,j); % Gets 
+                board_display(i,j+11) = shipPartType; %
             end
         end
     end
